4e7de5c418132ac87fe45f4507ad858d4eb02311
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-15 18:17:42 +0200

    shell: loops - while loop

--- simple_scripts/loops.sh
+++ simple_scripts/loops.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-for ((i = 0 ; i <= 100 ; i++)); do
-echo $i
+
+## For loop 
+# for ((i = 0 ; i <= 100 ; i++)); do
+# echo $i
+# done
+
+# While loop
+VAR=true
+while [ VAR=true ]
+do
+    echo "loop body here.."
+    VAR=false
+    break
 done
