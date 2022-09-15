5160f6ebed7294dc854e758306cee28da4c4fcbb
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-15 18:22:17 +0200

    shell: nested loop example

---
+++ simple_scripts/nested-loop-example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# nested loop example for printing numbers 1 - 10
+a=0
+while [ "$a" -lt 10 ]    # this is loop1
+do
+   b="$a"
+   while [ "$b" -ge 0 ]  # this is loop2
+   do
+      echo -n "$b "
+      b=`expr $b - 1`
+   done
+   echo
+   a=`expr $a + 1`
+done
